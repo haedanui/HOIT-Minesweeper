a1a1d75b613f35d2fc0d314c978c27e017973b8d
haedanui <101241360+haedanui@users.noreply.github.com>
2022-11-22 23:12:16 +0900

    Add files via upload

--- Field.java
+++ Field.java
@@ -1,13 +1,11 @@
 import java.awt.*;
 
-public class Field
-{
+public class Field {
 	private int x, y, neighbourMines;
 	private boolean clicked, mine, flag, mineClicked;
 	private Color color1, color2, color3, color4, color5, color6, color7, color8;
-	
-	public Field(int x, int y)
-	{
+
+	public Field(int x, int y) {
 		this.x = x;
 		this.y = y;
 		neighbourMines = 0;
@@ -21,77 +19,65 @@ public class Field
 		color8 = new Color(128, 128, 128);
 		flag = mineClicked = false;
 	}
-	
-	public boolean setClicked()
-	{
-		if (!flag)
-		{
+
+	public boolean setClicked() {
+		if (!flag) {
 			clicked = true;
 			mineClicked = mine;
 			return !mine;
 		}
 		return true;
 	}
-	
-	public boolean isClicked()
-	{
+
+	public boolean isClicked() {
 		return clicked;
 	}
-	
-	public void setMine(boolean mine)
-	{
+
+	public void setMine(boolean mine) {
 		this.mine = mine;
 	}
-	
-	public void incrementNeighbourMines()
-	{
+
+	public void incrementNeighbourMines() {
 		neighbourMines++;
 	}
-	
-	public int getNeighbourMines()
-	{
+
+	public int getNeighbourMines() {
 		return neighbourMines;
 	}
-	
-	public int setFlag()
-	{
+
+	public int setFlag() {
 		if (clicked)
 			return 0;
 		flag = !flag;
 		return flag ? -1 : 1;
 	}
-	
-	public boolean isFlag()
-	{
+
+	public boolean isFlag() {
 		return flag;
 	}
-	
-	public void setGameOver()
-	{
+
+	public void setGameOver() {
 		if (mine && !flag || !mine && flag)
 			clicked = true;
 	}
-	
-	public void setWon()
-	{
+
+	public void setWon() {
 		if (mine)
 			flag = true;
 	}
-	
-	public void paint(Graphics g)
-	{
+
+	public void paint(Graphics g) {
 		Color back = mineClicked ? Color.RED : Color.lightGray;
 		g.setColor(back);
 		g.fillRect(x * 16, y * 16, 16, 16);
-		
-		if (clicked)
-		{
+		g.setFont(new Font("굴림", Font.BOLD, 14));
+
+		if (clicked) {
 			g.setColor(Color.GRAY);
 			g.fillRect(x * 16, y * 16, 16, 1);
 			g.fillRect(x * 16, y * 16 + 1, 1, 15);
-			
-			if (mine || !mine && flag)
-			{
+
+			if (mine || !mine && flag) {
 				g.setColor(Color.BLACK);
 				g.fillRect(x * 16 + 4, y * 16 + 4, 9, 9);
 				g.fillRect(x * 16 + 2, y * 16 + 8, 13, 1);
@@ -99,25 +85,22 @@ public class Field
 
 				g.setColor(Color.WHITE);
 				g.fillRect(x * 16 + 6, y * 16 + 6, 2, 2);
-				
-				if (!mine && flag)
-				{
+
+				if (!mine && flag) {
 					g.setColor(Color.green);
 					g.fillRect(x * 16 + 1, y * 16 + 1, 15, 15);
 					g.setColor(Color.RED);
 					g.drawLine(x * 16 + 2, y * 16 + 2, x * 16 + 14, y * 16 + 14);
 					g.drawLine(x * 16 + 2, y * 16 + 14, x * 16 + 14, y * 16 + 2);
 				}
-				
+
 				return;
 			}
-			
-			switch (neighbourMines)
-			{
+
+			switch (neighbourMines) {
 				case 1:
-					g.setFont(new Font("굴림", Font.BOLD, 14));
 					g.setColor(color1);
-					g.drawString("1", x*16+5, y*16+13);
+					g.drawString("1", x * 16 + 5, y * 16 + 13);
 					break;
 				case 2:
 					g.setColor(color2);
@@ -148,16 +131,13 @@ public class Field
 					g.drawString("8", x * 16 + 5, y * 16 + 13);
 					break;
 			}
-		}
-		else
-		{
+		} else {
 			g.setColor(Color.WHITE);
 			g.fillRect(x * 16, y * 16, 16, 1);
 			g.fillRect(x * 16, y * 16, 1, 16);
-			if (flag)
-			{
+			if (flag) {
 				g.setColor(Color.green);
-				g.fillRect(x * 16+1, y * 16+1, 14, 14);
+				g.fillRect(x * 16 + 1, y * 16 + 1, 14, 14);
 
 			}
 		}
